b9d33b59305473da79721b1cf558a6944ad1c3d7
Luciano Albuquerque <lucianoreul@gmail.com>
2018-09-12 14:43:15 -0300

    Implement code

--- Assets/Scripts/FirebaseConnect.cs
+++ Assets/Scripts/FirebaseConnect.cs
@@ -1,16 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Firebase;
+using Firebase.Auth;
+using Firebase.Unity.Editor;
 
-public class FirebaseConnect : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
+public class FirebaseConnect : MonoBehaviour 
+{
+	
+    	private DependencyStatus dependencyStatus = DependencyStatus.UnavailableOther;
+    	private string appUrl = "";
+	
+	void Start ()
+	{
+		InitializeFirebase();
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    	protected virtual void InitializeFirebase()
+    	{
+		FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task =>
+		{
+		    dependencyStatus = task.Result;
+		    if (dependencyStatus == DependencyStatus.Available)
+		    {
+			FirebaseApp app = FirebaseApp.DefaultInstance;
+
+			// caminho para a inicialização no editor.
+			app.SetEditorDatabaseUrl(appUrl);
+			if (app.Options.DatabaseUrl != null)
+			{
+			    app.SetEditorDatabaseUrl(app.Options.DatabaseUrl);
+			}
+		    }
+		    else
+		    {
+			Debug.LogError("Não foi possivel resolver as dependêcias: " + dependencyStatus);
+		    }
+		});
+    	}
 }
