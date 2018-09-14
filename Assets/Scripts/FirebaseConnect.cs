8800e6a61d1d6e5a55c2ba6b275526a27997fd4c
Luciano Reul <lucianoreul@gmail.com>
2018-09-13 22:46:51 -0300

    Firebase integration applied

--- Assets/Scripts/FirebaseConnect.cs
+++ Assets/Scripts/FirebaseConnect.cs
@@ -13,10 +13,12 @@ public class FirebaseConnect : MonoBehaviour
 	
 	void Start ()
 	{
-        //InitializeFirebase();
-	}
-	
-    protected virtual void InitializeFirebase()
+        //InitializeFirebaseOnEditor();
+        InitializeFirebase();
+    }
+
+    // Inicia o firebase no unityeditor
+    protected virtual void InitializeFirebaseOnEditor()
     {
         FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task =>
         {
@@ -34,8 +36,47 @@ public class FirebaseConnect : MonoBehaviour
             }
             else
             {
-                Debug.LogError("Não foi possivel resolver as dependêcias: " + dependencyStatus);
+                Debug.LogError("Could not resolve all Firebase dependencies: " + dependencyStatus);
             }
+        });
+    }
+
+    protected virtual void InitializeFirebase()
+    {
+        FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task => 
+        {
+            var dependencyStatus = task.Result;
+            if (dependencyStatus == DependencyStatus.Available)
+            {
+                AuthenticateAnonymouslyUser();
+            }
+            else
+            {
+                Debug.LogError(System.String.Format(
+                  "Could not resolve all Firebase dependencies: {0}", dependencyStatus));
+                // Firebase Unity SDK is not safe to use here.
+            }
+        });
+    }
+
+    protected virtual void AuthenticateAnonymouslyUser()
+    {
+        FirebaseAuth.DefaultInstance.SignInAnonymouslyAsync().ContinueWith(task => 
+        {
+            if (task.IsCanceled)
+            {
+                Debug.LogError("SignInAnonymouslyAsync was canceled.");
+                return;
+            }
+            if (task.IsFaulted)
+            {
+                Debug.LogError("SignInAnonymouslyAsync encountered an error: " + task.Exception);
+                return;
+            }
+
+            FirebaseUser newUser = task.Result;
+            Debug.LogFormat("User signed in successfully: {0} ({1})",
+                newUser.DisplayName, newUser.UserId);
         });
     }
 }
